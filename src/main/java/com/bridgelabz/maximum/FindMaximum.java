6b147c948e57ee8e724508050746e4c7b08673a5
Yogi@gag <dr.yogeesh31@gmail.com>
2025-05-03 15:51:16 +0530

    [Yogeesh] Updated:added the methods to find the float maximum values only

--- src/main/java/com/bridgelabz/maximum/FindMaximum.java
+++ src/main/java/com/bridgelabz/maximum/FindMaximum.java
@@ -2,18 +2,6 @@ package com.bridgelabz.maximum;
 
 public class FindMaximum {
 
-    public Integer findMaximum(Integer num1,Integer num2,Integer num3){
-
-        Integer max=num1;
-        if(num2.compareTo(max)>0){
-            max=num2;
-        }
-        if(num3.compareTo(max)>0){
-            max=num3;
-        }
-        return max;
-    }
-
     public Float findFloatMaximum(Float num1,Float num2,Float num3){
 
         Float max=num1;
